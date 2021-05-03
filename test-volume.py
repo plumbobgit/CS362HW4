b583e484f3516f9bc24921fd1fb4aa63828cde3d
plumbobgit <plumbob@oregonstate.edu>
2021-05-02 19:30:49 -0700

    Fixed Volume Test

--- test-volume.py
+++ test-volume.py
@@ -5,7 +5,7 @@ class TestVolume(unittest.TestCase):
 
     def test-volume(self):
         self.assertAlmostEqual(volume.volume(2), 8)
-        self.assertAlmostEqual(volume.volume(-2), 8)
+        self.assertAlmostEqual(volume.volume(-2), -8)
         self.assertAlmostEqual(volume.volume(0), 0)
 
 if __name__ == '__main__':
