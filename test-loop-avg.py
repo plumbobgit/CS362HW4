ad9becb363957e17b077294b84ddfc93414a2fb4
plumbobgit <plumbob@oregonstate.edu>
2021-05-02 19:37:43 -0700

    Edited List Test

--- test-loop-avg.py
+++ test-loop-avg.py
@@ -5,7 +5,7 @@ class TestAverage(unittest.TestCase):
 
     def test-average(self):
         self.assertAlmostEqual(loop-avg.average([2, 2, 2]), 2)
-        self.assertAlmostEqual(loop-avg.average(2, -2, 2), .66)
+        self.assertAlmostEqual(loop-avg.average([2, -2, 2]), .66)
         self.assertAlmostEqual(loop-avg.average([0, 2, 2]), 1.33)
 
 if __name__ == '__main__':
